8778992641eaccfb240fd1944e77083ffe12f987
Christiana Razafindrasoa <ifalianarazafindrasoa@gmail.com>
2025-09-18 16:13:20 -0700

    Updated STT

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,16 +1,10 @@
 import whisper
 import ssl
+import json
 
 ssl._create_default_https_context = ssl._create_unverified_context
 model = whisper.load_model("tiny.en")
 result = model.transcribe("/original_podcast.mp3")
 
-timestamps = []
-texts = []
-
-for segment in result["segments"]:
-    timestamps.append(segment["end"])
-    texts.append(segment["text"])
-
-print(timestamps)
-print(texts)
+with open("whisper_output_example.json", "w") as f:
+    json.dump(result, f)
