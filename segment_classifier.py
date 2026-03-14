0d61de405c8de43a2b6a95b333a7bc7fe055ecd2
Christiana Razafindrasoa <ifalianarazafindrasoa@gmail.com>
2026-03-14 13:14:42 -0700

    updated packages

--- segment_classifier.py
+++ segment_classifier.py
@@ -12,7 +12,9 @@ ads = []
 for segment in data["segments"]:
     text = f" Classify this segment: {segment['text']}"
     result = classifier(text, labels)
-    classification = "ad segment" if result["labels"][0] == "commercial advertisement sponsor promotion" else "content segment"
-    
+    if result["labels"][0] == "commercial advertisement sponsor promotion":  #type: ignore
+        classification = "ad segment" 
+    else:
+        classification = "content segment"
     if classification == "ad segment":
         ads.append(segment["id"])
--- speech_to_text.py
+++ speech_to_text.py
@@ -1,10 +1,8 @@
 import whisper
-import ssl
 import json
 
-ssl._create_default_https_context = ssl._create_unverified_context
 model = whisper.load_model("tiny.en")
-result = model.transcribe("/original_podcast.mp3")
+result = model.transcribe("original_podcast.mp3")
 
 with open("samples/whisper_output_example.json", "w") as f:
     json.dump(result, f)
